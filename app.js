67ef1043f508009fdb01b5358e36c7bcfa465c78
Kacper Stępień <kacper2007x48@gmail.com>
2023-07-18 19:16:37 +0200

    change CORS policy

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(mongoSanitize());
 app.use(xss());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/animals", animalRouter);
